f972e79df6d5abc4f18d02ef15184df1b75a7e95
Alokesh13 <adpvlogs7@gmail.com>
2026-05-28 16:44:42 +0530

    Initialize Supabase client in supabase.js

---
+++ src/lib/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://otfekpekdqhcnklyxaoq.supabase.co/rest/v1/',
+  'sb_publishable_eRNkd1xFY-0Oi8bcyH4gNw_rS-QqZkr'
+)
